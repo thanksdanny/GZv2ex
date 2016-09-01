bde0f483f7aeee84b7f1cf40c9b20f4171cdf0c4
thanksdanny <hj-07@163.com>
2016-09-01 13:22:25 +0800

    Initial Commit

---
+++ GZv2ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZv2ex
+//
+//  Created by Danny Ho on 9/1/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
